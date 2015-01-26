e5cf53e02ce8b3930a5665bc58e05070f76fd4df
hugo <hugo@soixantecircuits.fr>
2015-01-27 00:18:53 +0100

    change accelerometer model

--- poufTweet.ino
+++ poufTweet.ino
@@ -1,63 +1,33 @@
+#include <Wire.h>
 #include <Bridge.h>
 #include <HttpClient.h>
+#include "MMA7660.h"
 
-// http://localhost:3000/twitt/?scrname=hugohil&pufNum=1
-
-int xInput = A0;
-int yInput = A1;
-int zInput = A2;
-
-int xPrev = 0;
-int yPrev = 0;
-int zPrev = 0;
-
-// Raw Ranges:
-// initialize to mid-range and allow calibration to
-// find the minimum and maximum for each axis
-int xRawMin = 512;
-int xRawMax = 512;
-
-int yRawMin = 512;
-int yRawMax = 512;
-
-int zRawMin = 512;
-int zRawMax = 512;
-
-const int sampleSize = 10;
+MMA7660 accelemeter;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  accelemeter.init();
+
   Bridge.begin();
-  digitalWrite(13, HIGH);
 }
 
 void loop() {
   HttpClient client;
 
-  int xRaw = analogRead(xInput);
-  int yRaw = analogRead(yInput);
-  int zRaw = analogRead(zInput);
+  float ax,ay,az;
+  accelemeter.getAcceleration(&ax,&ay,&az);
+  // Serial.print(abs(ax));
+  // Serial.print(", ");
+  // Serial.print(abs(ay));
+  // Serial.print(", ");
+  // Serial.println(abs(az));
 
-  if((abs(xRaw) - abs(xPrev)) + (abs(yRaw) - abs(yPrev)) + (abs(zRaw) - abs(zPrev)) > 10){
-    Serial.print(xRaw);
-    Serial.print(", ");
-    Serial.print(yRaw);
-    Serial.print(", ");
-    Serial.println(zRaw);
-    client.get("http://192.168.1.16:3000/twitt/?scrname=hugohil&pufNum=1");
-    while (client.available()) {
-      char c = client.read();
-      Serial.print(c);
-    }
-    Serial.flush();
+  if(abs(ax) + abs(ay) + abs(az) > 1.8){
+    Serial.println("send");
+    client.get("http://cybertron.meteor.com/twitt/?scrname=NormanQCtheBot&pufNum=7");
   }
 
-  xPrev = xRaw;
-  yPrev = yRaw;
-  zPrev = zRaw;
-
-  delay(100);
+  delay(50);
 }
