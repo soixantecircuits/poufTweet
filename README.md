fc786c41d079c3837cc2f0e282fabc88e6bd1834
hugo <hugo@soixantecircuits.fr>
2015-01-27 15:13:17 +0100

    New accelerometer

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Sit on a pouffe, it tweets for you.
 
 Works on an Arduino Yùn with [Twitter Gateway](https://github.com/soixantecircuits/twitter-gateway).
+
+# Get the Acceloremeter lib
+
+https://github.com/Seeed-Studio/Accelerometer_MMA7660
--- poufTweet.ino
+++ poufTweet.ino
@@ -4,6 +4,13 @@
 #include "MMA7660.h"
 
 MMA7660 accelemeter;
+long lastDebounceTime = 0;  // the last time the output pin was toggled
+long debounceDelay = 1000;    // the debounce time; increase if the output flickers
+
+  int8_t x;
+  int8_t y;
+  int8_t z;
+  int8_t sum;
 
 void setup() {
   Serial.begin(9600);
@@ -15,19 +22,32 @@ void setup() {
 
 void loop() {
   HttpClient client;
+  accelemeter.getXYZ(&x,&y,&z);
+  /*
+  Serial.print("x = ");
+  Serial.println(x);
+  Serial.print("y = ");
+  Serial.println(y);
+  Serial.print("z = ");
+  Serial.println(z);
+  */
 
-  float ax,ay,az;
-  accelemeter.getAcceleration(&ax,&ay,&az);
+  //accelemeter.getAcceleration(&ax,&ay,&az);
   // Serial.print(abs(ax));
   // Serial.print(", ");
   // Serial.print(abs(ay));
   // Serial.print(", ");
   // Serial.println(abs(az));
+  sum = abs(x) + abs(y) + abs(z);
+  Serial.println(sum);
 
-  if(abs(ax) + abs(ay) + abs(az) > 1.8){
+  if( sum > 50 && (millis() - lastDebounceTime) > debounceDelay){
     Serial.println("send");
+    lastDebounceTime = millis();
+    //client.get("http://cybertron.meteor.com/twitt/?scrname=WilliamTheBot");
     client.get("http://cybertron.meteor.com/twitt/?scrname=NormanQCtheBot&pufNum=7");
   }
+
 
   delay(50);
 }
